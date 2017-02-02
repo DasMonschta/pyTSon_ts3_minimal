79243581476993eb4a3adad4eeed85aacb7831ed
Metvernichter <metvernichter.github@gmail.com>
2017-02-02 11:55:22 +0100

    first commit

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -2,17 +2,17 @@ from ts3plugin import ts3plugin, PluginHost
 import ts3defines, ts3lib
 
 class Minimal(ts3plugin):
-    name 				= "Minimal example"
-    requestAutoload 	= False
-    version 			= "1.0"
-    apiVersion 			= 21
-    author 				= "your name"
-    description 		= "a description"
-    offersConfigure		= False
-    commandKeyword 		= ""
-    infoTitle			= None
-    hotkeys 			= []
-    menuItems           = []
+    name = "Minimal example"
+    requestAutoload = False
+    version = "1.0"
+    apiVersion = 21
+    author = "your name"
+    description = "a description"
+    offersConfigure = False
+    commandKeyword = ""
+    infoTitle = None
+    hotkeys = []
+    menuItems = []
 
     def __init__(self):
         pass
